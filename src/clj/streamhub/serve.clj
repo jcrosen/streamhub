7e526d5ce5a3caeb628e130e6bc96d591eaf89d6
Jeremy Crosen <jeremy.crosen@gmail.com>
2015-07-12 16:09:28 -0700

    Add stream publishing endpoint

--- src/clj/streamhub/serve.clj
+++ src/clj/streamhub/serve.clj
@@ -6,9 +6,10 @@
             [compojure.response :refer [render]]
             [org.httpkit.server :as s]
             [clojure.core.async :as async :refer [<! >! go-loop]]
-            [streamhub.stream :refer [subscribe-to-stream! close-subscription!]]))
+            [streamhub.stream :refer [start-stream! publish-stream! close-stream!
+                                      subscribe-to-stream! close-subscription! gen-stream]]))
 
-(defn gen-stream-handler [context stream-id]
+(defn gen-stream-subscription [context stream-id]
   (let [!streams (context :!streams)
         sub-chan (async/chan)
         sub-id (subscribe-to-stream! !streams stream-id sub-chan)]
@@ -22,15 +23,32 @@
           (s/on-close web-chan (fn [status]
                                 (close-subscription! !streams stream-id sub-id)
                                 (async/close! sub-go-chan)
-                                (println "web-chan closed: " status)))
+                                (println "Subscription closed: " status)))
           (s/on-receive web-chan (fn [data]
                                   (s/send! web-chan data))))))))
+
+(defn gen-stream-publication [context stream-data]
+  (let [!streams (context :!streams)
+        stream (gen-stream :metadata stream-data)
+        pub-chan (stream :chan)
+        stream-id (stream :id)]
+    (println (str "New client publisher: " (stream :id)))
+    (fn [request]
+      (start-stream! !streams stream-id)
+      (publish-stream! !streams stream)
+      (s/with-channel request web-chan
+        (s/on-close web-chan (fn [status]
+                              (close-stream! !streams stream-id)
+                              (println "Publication closed: " status)))
+        (s/on-receive web-chan (fn [data]
+                                  (async/put! pub-chan data)))))))
 
 (defn gen-routes [context]
   (routes
     (GET "/" req (str req))
     (GET "/streams" req (str "<html><head><script type='text/javascript' src='/js/main.js'></script></head><body>Streams:\n" @(context :!streams) "</body></html>"))
-    (GET "/stream/:stream-id" [stream-id] (gen-stream-handler context stream-id))
+    (GET "/stream/subscribe/:stream-id" [stream-id] (gen-stream-subscription context stream-id))
+    (GET "/stream/publish" req (gen-stream-publication context (req :stream-data)))
     (cmpr/not-found "404 - Not Found")))
 
 (defn gen-handler [context]
